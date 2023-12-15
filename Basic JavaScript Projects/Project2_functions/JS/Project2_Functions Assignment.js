1c4f95c0ae5c73267fa4a73e5dd3155da5a7a7a0
CBV182 <125630047+CBV182@users.noreply.github.com>
2023-12-15 05:37:41 +0000

    Project2_Functions Assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/Project2_Functions Assignment.js
@@ -0,0 +1,10 @@
+
+//Function
+
+function myfunction(){
+    var sentence = "I took a test in existentialism."; //varible
+    sentence += " I left all the answers blank and got 100.-Woody Allen"; //+=operator
+    document.getElementById("concatenate").innerHTML= sentence; //document.getElementById
+    concatenate.style.color="blue"; //Styling 
+
+}
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+
+
+function My_First_Function() {
+    var str="This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
