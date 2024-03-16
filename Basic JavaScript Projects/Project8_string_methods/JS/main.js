68c95e6222443a37c058665e6ca72ddee8c75f2d
CBV182 <125630047+CBV182@users.noreply.github.com>
2024-03-16 03:40:54 +0000

    slice() Method Assignment

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,3 +1,5 @@
+/*concat() Method*/
+
 function Full_Sentence(){
     var part_1 = "Group ";
     var part_2 = "of words ";
@@ -5,4 +7,12 @@ function Full_Sentence(){
     var part_4 = "together.";
     var whole_sentence = part_1.concat(part_2,part_3,part_4);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+/*slice() Method*/
+
+function slice_Method(){
+    var Sentence = "Trust the magic of new beginnings.";
+    var Section = Sentence.slice(10,15);
+    document.getElementById("Slice").innerHTML = Section;
 }
